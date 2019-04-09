62690b3d7241d6321f1478babae318a73819dd82
ssoso27 <sohi8290@naver.com>
2019-04-10 04:06:14 +0900

    [F3] 각 화면 생성 및 연결
    - 메뉴 UI 라이브러리 연결 (길로틴메뉴)

--- app/src/main/java/com/example/welcare_catcher/MainActivity.java
+++ app/src/main/java/com/example/welcare_catcher/MainActivity.java
@@ -3,31 +3,55 @@ package com.example.welcare_catcher;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+
+import com.yalantis.guillotine.animation.GuillotineAnimation;
 
 public class MainActivity extends AppCompatActivity {
+    private static final long RIPPLE_DURATION = 250;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button.OnClickListener onClickListener = new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                switch (view.getId()) {
-                    case R.id.btn_facility_list :
-                        Intent intent = new Intent(MainActivity.this, FacilityListActivity.class) ;
-                        startActivity(intent) ;
-                        break;
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        FrameLayout root = findViewById(R.id.root);
+        View contentHamburger = findViewById(R.id.content_hamburger);
 
-                    default:
-                        break;
-                }
-            }
-        };
+//        if (toolbar != null) {
+//            setSupportActionBar(toolbar);
+//            getSupportActionBar().setTitle(null);
+//        }
 
-        findViewById(R.id.btn_facility_list).setOnClickListener(onClickListener);
+        View guillotineMenu = LayoutInflater.from(this).inflate(R.layout.guillotine, null);
+        root.addView(guillotineMenu);
+
+        new GuillotineAnimation.GuillotineBuilder(guillotineMenu, guillotineMenu.findViewById(R.id.guillotine_hamburger), contentHamburger)
+                .setStartDelay(RIPPLE_DURATION)
+                .setActionBarViewForAnimation(toolbar)
+                .setClosedOnStart(true)
+                .build();
+//
+//        Button.OnClickListener onClickListener = new Button.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                switch (view.getId()) {
+//                    case R.id.btn_facility_list :
+//                        Intent intent = new Intent(MainActivity.this, FacilityListActivity.class) ;
+//                        startActivity(intent) ;
+//                        break;
+//
+//                    default:
+//                        break;
+//                }
+//            }
+//        };
+//
+//        findViewById(R.id.btn_facility_list).setOnClickListener(onClickListener);
     }
 }
