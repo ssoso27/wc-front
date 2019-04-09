5e485144659c0f8c635af7e299cde685907d291d
ssoso27 <sohi8290@naver.com>
2019-04-10 06:33:08 +0900

    [F3] 각 화면 생성 및 연결
    - 메인 화면

--- app/src/main/java/com/example/welcare_catcher/SettingFragment.java
+++ app/src/main/java/com/example/welcare_catcher/SettingFragment.java
@@ -15,6 +15,6 @@ public class SettingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.frag_selfcheck, container, false);
+        return inflater.inflate(R.layout.frag_setting, container, false);
     }
 }
