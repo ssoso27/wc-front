6e9c214526678b426fba6e09ebfcbc30a18b7ed3
ssoso27 <sohi8290@naver.com>
2019-04-10 02:26:19 +0900

    [F3] 각 화면 생성 및 연결
    - 복지시설 목록 화면생성 및 연결

--- app/src/main/java/com/example/welcare_catcher/MainActivity.java
+++ app/src/main/java/com/example/welcare_catcher/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.welcare_catcher;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button.OnClickListener onClickListener = new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.btn_facility_list :
+                        Intent intent = new Intent(MainActivity.this, FacilityListActivity.class) ;
+                        startActivity(intent) ;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        };
+
+        findViewById(R.id.btn_facility_list).setOnClickListener(onClickListener);
     }
 }
