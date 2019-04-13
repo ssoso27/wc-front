7d0a5c6e51406c2e33e1d1426e35b962bb304ba0
ssoso27 <sohi8290@naver.com>
2019-04-14 04:58:34 +0900

    [F15] 카카오 계정 로그인 API 연동
    - 로그인 성공

--- app/src/main/java/com/uuay/welcare_catcher/AccountFragment.java
+++ app/src/main/java/com/uuay/welcare_catcher/AccountFragment.java
@@ -1,23 +1,72 @@
 package com.uuay.welcare_catcher;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.uuay.welcare_catcher.R;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.usermgmt.LoginButton;
+import com.kakao.util.exception.KakaoException;
+import com.kakao.util.helper.log.Logger;
 
 public class AccountFragment extends Fragment {
+    private SessionCallback callback;
     public AccountFragment() {
 
+    }
+
+    /*
+     * 로그인 버튼을 클릭 했을시 access token을 요청하도록 설정한다.
+     *
+     * @param savedInstanceState 기존 session 정보가 저장된 객체
+     */
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frag_account, container, false);
 
-        return inflater.inflate(R.layout.frag_account, container, false);
+        LoginButton loginButton = view.findViewById(R.id.com_kakao_login);
+        loginButton.setSuportFragment(this); // set fragment for LoginButton
+
+        callback = new SessionCallback();
+        Session.getCurrentSession().addCallback(callback);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Session.getCurrentSession().removeCallback(callback);
+    }
+
+    private class SessionCallback implements ISessionCallback {
+
+        @Override
+        public void onSessionOpened() {
+//            final Intent intent = new Intent(getActivity(), SampleSignupActivity.class);
+//            startActivity(intent);
+            Log.d("SessionCallback", "성공!");
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            if(exception != null) {
+                Logger.e(exception);
+            }
+        }
     }
 }
