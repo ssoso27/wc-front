949e7a12029ab7048e1acc4764f09205052c4a67
parkhyunkyu <parkhyun52@gmail.com>
2019-04-16 00:12:05 +0900

    [F27] 복지 목록 화면 구현 난잡..

---
+++ app/src/main/java/com/uuay/welcare_catcher/WelfareListEdu.java
@@ -0,0 +1,24 @@
+package com.uuay.welcare_catcher;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WelfareListEdu extends Fragment {
+    public WelfareListEdu() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.frag_welfare_edu, container, false);
+    }
+}
---
+++ app/src/main/java/com/uuay/welcare_catcher/WelfareListPrivate.java
@@ -0,0 +1,25 @@
+package com.uuay.welcare_catcher;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WelfareListPrivate extends Fragment {
+    public WelfareListPrivate() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.frag_welfare_private, container, false);
+    }
+}
