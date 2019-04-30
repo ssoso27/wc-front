6d53553919aafde5066984362627e411efe2934f
ssoso27 <sohi8290@naver.com>
2019-05-01 07:55:34 +0900

    [F36] 전반적인 디자인 변경
    - 배경색 gray0로 통일

--- app/src/main/java/com/uuay/welcare_catcher/view/AccountFragment.java
+++ app/src/main/java/com/uuay/welcare_catcher/view/AccountFragment.java
@@ -35,7 +35,7 @@ public class AccountFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.frag_account, container, false);
+        View view = inflater.inflate(R.layout.frag_login, container, false);
 
         LoginButton loginButton = view.findViewById(R.id.com_kakao_login);
         loginButton.setSuportFragment(this); // set fragment for LoginButton
