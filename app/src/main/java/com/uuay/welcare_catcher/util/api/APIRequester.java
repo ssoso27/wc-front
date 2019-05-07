6076c2be6d542d068352c9e7d6a3b85d2c0e449e
ssoso27 <sohi8290@naver.com>
2019-05-07 23:44:12 +0900

    [F38] 시설명 검색 연결
    - 검색 연결 성공

--- app/src/main/java/com/uuay/welcare_catcher/util/api/APIRequester.java
+++ app/src/main/java/com/uuay/welcare_catcher/util/api/APIRequester.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import com.uuay.welcare_catcher.model.Account;
 import com.uuay.welcare_catcher.model.Facility;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,24 +16,37 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIRequester {
-    private Retrofit retrofit;
     private RetrofitAPI retrofitAPI;
-    private Call<String> stringCall;
-    private Call<List<Facility>> facilityListCall;
-    private ArrayList<Facility> facilities;
 
     public APIRequester() {
         this.setRetrofit();
     }
 
     public void join(Account account) {
-        stringCall = retrofitAPI.join(account);
+        Call<String> stringCall = retrofitAPI.join(account);
         stringCall.enqueue(stringCallback);
     }
 
-    public void findFacilities(String keyword, int size, int page) {
-        facilityListCall = retrofitAPI.findFacilities(keyword, size, page);
-        facilityListCall.enqueue(facilityListCallback);
+    public ArrayList<Facility> findFacilities(String keyword, int size, int page) {
+        ArrayList<Facility> facilities = null;
+
+        try {
+            Call<List<Facility>> call = retrofitAPI.findFacilities(keyword, size, page);
+            Response<List<Facility>> response = call.execute();
+
+            if(response.isSuccessful()) {
+                ArrayList<Facility> list = (ArrayList<Facility>) response.body();
+                if (list != null) {
+                    facilities = list;
+                }
+            }
+
+            return facilities;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     private Callback<String> stringCallback = new Callback<String>() {
@@ -51,38 +65,12 @@ public class APIRequester {
         }
     };
 
-    private Callback<List<Facility>> facilityListCallback = new Callback<List<Facility>>() {
-
-        @Override
-        public void onResponse(Call<List<Facility>> call, Response<List<Facility>> response) {
-            int code = response.code();
-            Log.d("onResponse", String.valueOf(code));
-
-            if(response.isSuccessful()) {
-                ArrayList<Facility> list = (ArrayList<Facility>) response.body();
-                if (list != null) {
-                    facilities = list;
-                }
-            }
-        }
-
-        @Override
-        public void onFailure(Call<List<Facility>> call, Throwable t) {
-            Log.d("onFailure", "으나야사랑해ㅜ");
-            t.printStackTrace();
-        }
-    };
-
     private void setRetrofit() {
-        retrofit = new Retrofit.Builder()
+        Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://54.180.147.7")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         retrofitAPI = retrofit.create(RetrofitAPI.class);
-    }
-
-    public ArrayList<Facility> getFacilities() {
-        return facilities;
     }
 }
