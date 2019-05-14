68e0c62a6a1d9d2a90474143944e363513488192
박현규 <parkhyun52@gmail.com>
2019-05-15 07:10:47 +0900

    [F18] 자가진단 화면 구현 - 항목별로 이벤트 작성 - "저장하기" 버튼 클릭 이벤트로 값들 넘겨주는 거 만들 거임

--- app/src/main/java/com/uuay/welcare_catcher/view/SelfcheckFragment.java
+++ app/src/main/java/com/uuay/welcare_catcher/view/SelfcheckFragment.java
@@ -2,50 +2,182 @@ package com.uuay.welcare_catcher.view;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.uuay.welcare_catcher.R;
 
 public class SelfcheckFragment extends Fragment {
-
+    String gender, val, type, grade, area;
+    int age;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.frag_selfcheck, container, false);
+        final View view = inflater.inflate(R.layout.frag_selfcheck, container, false);
 
-        TextView firstTextView = (TextView) view.findViewById(R.id.first_textView1);
-        Button firstBtn1 = (Button) view.findViewById(R.id.first_btn1);
-        Button firstBtn2 = (Button) view.findViewById(R.id.first_btn2);
-        Button firstBtn3 = (Button) view.findViewById(R.id.first_btn3);
+        Spinner typeSpinner = (Spinner) view.findViewById(R.id.spinner_disability_type);
+        ArrayAdapter typeAdapter = setSpinner(typeSpinner);
 
-        TextView secondTextView = (TextView) view.findViewById(R.id.second_textView1);
-        Button secondBtn1 = (Button) view.findViewById(R.id.second_btn1);
-        Button secondBtn2 = (Button) view.findViewById(R.id.second_btn2);
-        Button secondBtn3 = (Button) view.findViewById(R.id.second_btn3);
+        Spinner gradeSpinner = (Spinner) view.findViewById(R.id.spinner_disability_grade);
+        ArrayAdapter gradeAdapter = setSpinner(gradeSpinner);
 
-        TextView thirdTextView = (TextView) view.findViewById(R.id.third_textView1);
-        Button thirdBtn1 = (Button) view.findViewById(R.id.third_btn1);
-        Button thirdBtn2 = (Button) view.findViewById(R.id.third_btn2);
-        Button thirdBtn3 = (Button) view.findViewById(R.id.third_btn3);
+        Spinner areaSpinner = (Spinner) view.findViewById(R.id.spinner_area);
+        ArrayAdapter areaAdapter = setSpinner(areaSpinner);
 
-        firstTextView.setText("1. 문항 ");
-        firstBtn1.setText("선택문항");
-        firstBtn2.setText("선택문항");
-        firstBtn3.setText("선택문항");
+        RadioGroup rgGender = (RadioGroup) view.findViewById(R.id.rg_gender);
 
-        secondTextView.setText("2. 문항");
-        secondBtn1.setText("선택문항");
-        secondBtn2.setText("선택문항");
-        secondBtn3.setText("선택문항");
+        rgGenderClick(rgGender);
 
-        thirdTextView.setText("2. 문항");
-        thirdBtn1.setText("선택문항");
-        thirdBtn2.setText("선택문항");
-        thirdBtn3.setText("선택문항");
+        EditText etAge = (EditText) view.findViewById(R.id.et_age);
+
+        etAgeClick(etAge);
+
+        typeSelected(typeSpinner);
+        gradeSelected(gradeSpinner);
+        areaSelected(areaSpinner);
+//        Button btnAddQuestion = (Button) view.findViewById(R.id.btn_add_question);
+//        btnAddQuestion.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+
+        Button btnEnd = (Button) view.findViewById(R.id.btn_selfcheck_end);
+        btnClick(btnEnd);
+
         return view;
+    }
+
+    public void rgGenderClick(RadioGroup radioGroup) {
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = (RadioButton) getView().findViewById(checkedId);
+                gender = radioButton.getText().toString();
+                Toast.makeText(SelfcheckFragment.super.getContext(), "성별 : " + gender,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public void etAgeClick(EditText editText) {
+        final EditText fEditText = editText;
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH ||
+                        actionId == EditorInfo.IME_ACTION_DONE ||
+                        event.getAction() == KeyEvent.ACTION_DOWN &&
+                                event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    age = Integer.valueOf(fEditText.getText().toString());
+                    Toast.makeText(SelfcheckFragment.super.getContext(), "나이 : " + age,
+                            Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    public void typeSelected(Spinner spinner) {
+            final Spinner fSpinner = spinner;
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    type = fSpinner.getSelectedItem().toString();
+                    Toast.makeText(SelfcheckFragment.super.getContext(), "장애유형 : " + type,
+                            Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+    }
+
+    public void gradeSelected(Spinner spinner) {
+        final Spinner fSpinner = spinner;
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                grade = fSpinner.getSelectedItem().toString();
+                Toast.makeText(SelfcheckFragment.super.getContext(), "장애등급 : " + grade,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    public void areaSelected(Spinner spinner) {
+        final Spinner fSpinner = spinner;
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                area = fSpinner.getSelectedItem().toString();
+                Toast.makeText(SelfcheckFragment.super.getContext(), "거주지역 : " + area,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    // 클래스명 짓는 거 ...
+    public void btnClick(Button btn) {
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public ArrayAdapter setSpinner(Spinner spinner) {
+        ArrayAdapter arrAdapter = null;
+        switch(spinner.getId()) {
+            case R.id.spinner_disability_type:
+                arrAdapter = ArrayAdapter.createFromResource(getContext(),
+                        R.array.sc_question4, android.R.layout.simple_spinner_item);
+                arrAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(arrAdapter);
+                return arrAdapter;
+
+            case R.id.spinner_disability_grade:
+                arrAdapter = ArrayAdapter.createFromResource(getContext(),
+                        R.array.sc_question5, android.R.layout.simple_spinner_item);
+                arrAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(arrAdapter);
+                return arrAdapter;
+
+            case R.id.spinner_area:
+                arrAdapter = ArrayAdapter.createFromResource(getContext(),
+                        R.array.sc_question6, android.R.layout.simple_spinner_item);
+                arrAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(arrAdapter);
+                return arrAdapter;
+
+            default:
+                return arrAdapter;
+        }
     }
 }
