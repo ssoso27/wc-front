64d693547e071957ff1120029146f23b586ab316
ssoso27 <sohi8290@gmail.com>
2019-06-12 03:48:08 +0900

    [F63] 로그인 여부에 따라 로그인/프로필 화면 보여주기
    - LocalCookie 생성

---
+++ app/src/main/java/com/uuay/welcare_catcher/util/LocalCookie.java
@@ -0,0 +1,28 @@
+package com.uuay.welcare_catcher.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LocalCookie {
+    private static Map<String, String> cookies;
+
+    private LocalCookie() {
+        cookies = new HashMap<String, String>();
+    }
+
+    private static class LocalCookieHolder {
+        public static final LocalCookie INSTANCE = new LocalCookie();
+    }
+
+    public static LocalCookie getInstance() {
+        return LocalCookieHolder.INSTANCE;
+    }
+
+    public void put(String key, String value) {
+        cookies.put(key, value);
+    }
+
+    public String get(String key) {
+        return cookies.get(key);
+    }
+}
